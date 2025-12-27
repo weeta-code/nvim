0e7263994df1c6b0e1d1f1d801ffbea5cb9a1859
weeta-code <victordesouza132@gmail.com>
2025-12-27 08:38:37 -0500

    copilot done

--- lua/victor/plugins/copilot.lua
+++ lua/victor/plugins/copilot.lua
@@ -1,4 +1,13 @@
+vim.g.copilot_no_tab_map = true
+
+vim.keymap.set("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.keymap.set("i", "<C-j>", 'copilot#Previous()', { silent = true, expr = true })
+vim.keymap.set("i", "<C-k>", 'copilot#Next()', { silent = true, expr = true })
+vim.keymap.set("i", "<C-l>", 'copilot#Dismiss()', { silent = true, expr = true })
+
 return {
   "github/copilot.vim",
+  cmd = "Copilot",
   event = "InsertEnter",
 }
